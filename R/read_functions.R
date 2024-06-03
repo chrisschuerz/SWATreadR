999f57e2d49db64c34f19c3fe9ddc807f5c16cc4
user.email <christoph.schuerz@ufz.de>
2024-06-03 16:47:05 +0200

    First implementation of read functions
    - A set of functions to read different file structures
      implemented
    - A lookup for different file structures implemented
    - read_swat tested for a few SWAT+ input/output files

---
+++ R/read_functions.R
@@ -0,0 +1,286 @@
+#' Read a SWAT+ input file which has a tabular structure.
+#'
+#' @param file_path Path of the SWAT+ input file.
+#' @param col_names (optional) Column names vector.
+#' @param n_skip Number of header rows to skip. Default is 1.
+#'
+#' @returns The SWAT+ input file as a tibble.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr recode %>%
+#' @importFrom purrr map2_df
+#' @importFrom tibble add_column tibble
+#'
+#' @keywords internal
+#'
+read_tbl <- function(file_path, col_names = NULL, col_types = NULL, n_skip = 1,
+                     has_unit = FALSE) {
+  if (file.exists(file_path)) {
+    tbl <- fread(file_path, skip = n_skip + 1 + has_unit, header = FALSE)
+    if (is.null(col_names)) {
+      col_names <- fread(file_path, skip = n_skip, nrows = 1, header = F) %>%
+        unlist(.) %>%
+        unname(.) %>%
+        add_suffix_to_duplicate(.)
+    }
+    if ('description' %in% col_names & ncol(tbl) == length(col_names) - 1) {
+      tbl <- add_column(tbl, description = '')
+    } else if (ncol(tbl) > length(col_names)) {
+      col_names_add <- paste0('v_', 1:(ncol(tbl) - length(col_names)))
+      col_names <- c(col_names, col_names_add)
+      warning("Number of columns of '", basename(file_path),"' > column names.\n",
+              "Column names ", paste(col_names_add, collapse = ', '),
+              ' were assigned to columns at the end.')
+    } else if (ncol(tbl) < length(col_names)) {
+      col_names_rmv <- col_names[(ncol(tbl) + 1):length(col_names)]
+      col_names <- col_names[1:ncol(tbl)]
+      warning("Number of columns of '", basename(file_path),"' < column names.\n",
+              "Column names ", paste(col_names_rmv, collapse = ', '),
+              ' were removed.')
+    }
+
+    names(tbl) <- col_names
+    tbl <- tibble(tbl)
+  } else {
+    if (is.null(col_names)) {
+      stop("File '", basename(file_path), "' does not exist and no 'col_names' ",
+           'were provided to generate empty table.')
+    }
+
+    tbl <- tibble(!!!rep(NA, length(col_names)),
+                  .rows = 0, .name_repair = ~ col_names)
+  }
+
+  if(!is.null(col_types)) {
+    col_types <- unlist(strsplit(col_types, '')) %>%
+      recode(., c = 'character', d = 'numeric', i = 'integer')
+    tbl <- map2_df(tbl, col_types, ~ as(.x, .y))
+  }
+
+  return(tbl)
+}
+
+#' Read a SWAT+ input file which has a tabular structure with a definition line
+#' for each parameter table section (e.g. management.sch, plant.ini, soils.sol).
+#'
+#' @param file_path Path of the SWAT+ input file.
+#' @param def_names Vector of column names for the entries in the definition
+#'   line.
+#' @param par_names Vector of parameter names of the parameter table.
+#' @param id_num ID vector to define the columns which are numerical values.
+#'
+#' @returns The SWAT+ management.sch input file as a tibble.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr bind_rows bind_cols mutate %>%
+#' @importFrom purrr map map_int map_chr map2 map2_df map_df set_names
+#' @importFrom stringr str_replace_all str_trim str_split
+#' @importFrom tibble as_tibble
+#'
+#' @keywords internal
+#'
+read_tbl2 <- function(file_path, def_names, par_names, id_num = NULL) {
+  n_def <- length(def_names)
+  n_par <- length(par_names)
+
+  file_line <- fread(file_path, skip = 2, sep = NULL, sep2 = NULL,
+                     header = FALSE) %>%
+    unlist(.) %>%
+    unname(.) %>%
+    str_trim(.) %>%
+    str_replace_all(., '\t', ' ') %>%
+    str_split(., '[:space:]+')
+
+  n_elem <- map_int(file_line, length)
+  file_line  <- file_line[n_elem != 1]
+  n_elem <- n_elem[n_elem != 1]
+  def_pos <- which(n_elem == n_def)
+
+  pos_start <- def_pos + 1
+  pos_end <- c(def_pos[2:length(def_pos)] - 1, length(file_line))
+  no_entry <- pos_start > pos_end
+  pos_start[no_entry] <- length(file_line) + 1
+  pos_end[no_entry] <- length(file_line) + 1
+
+  par_tbl <- map2(pos_start, pos_end, ~ file_line[.x:.y]) %>%
+    map(., unlist) %>%
+    map(., ~ as_mtx_null(.x, n_par)) %>%
+    map(., ~ as_tibble(.x, .name_repair = ~ par_names))
+
+  n_op <- map_int(par_tbl, nrow)
+
+  par_tbl <- bind_rows(par_tbl)
+
+  def_tbl <- map(def_pos, ~ file_line[[.x]]) %>%
+    map(., unlist) %>%
+    map(., ~ as_mtx_null(.x, n_def)) %>%
+    map(., ~ as_tibble(.x, .name_repair = ~ def_names)) %>%
+    map2(., n_op, ~ .x[rep(1, .y), ]) %>%
+    bind_rows(.)
+
+  tbl <- bind_cols(def_tbl, par_tbl)
+
+  if(!is.null(id_num)) {
+    tbl[,id_num] <- map_df(tbl[,id_num], as.numeric)
+  }
+
+  return(tbl)
+}
+
+#' Read the n column of a tabular SWAT+ input file which are defined by the
+#' column positions `id_col_sel`. This is useful if e.g. last columns with
+#' description cause issues with reading due to blanks in the description text.
+#'
+#' @param file_path Path of the SWAT+ input file.
+#' @param col_names (optional) Character column names vector.
+#' @param n_skip Number of header rows to skip. Default is 1.
+#' @param id_col_sel Numeric vector which defines the column positions that are
+#'   returned in the table.
+#' @param id_num ID vector to define the columns which are numerical values.
+#'
+#' @returns The SWAT+ management.sch input file as a tibble.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr bind_rows bind_cols mutate %>%
+#' @importFrom purrr map map_chr map_df
+#' @importFrom stringr str_replace_all str_trim str_split
+#' @importFrom tibble as_tibble
+#'
+#' @keywords internal
+#'
+read_tbl_n <- function(file_path, col_names = NULL, n_skip = 1,
+                       id_col_sel = NULL, id_num = NULL) {
+  if (is.null(col_names)) {
+    col_names <- fread(file_path, skip = n_skip, nrows = 1, header = F) %>%
+      unlist(.) %>%
+      unname(.) %>%
+      add_suffix_to_duplicate(.)
+    if (!is.null(id_col_sel)) {
+      col_names <- col_names[id_col_sel]
+    }
+  }
+
+  file_line <- fread(file_path, skip = n_skip + 1, sep = NULL, sep2 = NULL,
+                     header = FALSE) %>%
+    unlist(.) %>%
+    unname(.) %>%
+    str_trim(.) %>%
+    str_replace_all(., '\t', ' ') %>%
+    str_split(., '[:space:]+')
+
+  if (!is.null(id_col_sel)) {
+    file_line <- map(file_line, ~ .x[id_col_sel])
+  }
+
+  tbl <- file_line %>%
+    map(., unlist) %>%
+    map(., ~ as_mtx_null(.x, length(file_line[[1]]))) %>%
+    map_df(., ~ as_tibble(.x, .name_repair = ~ col_names))
+
+  if(!is.null(id_num)) {
+    tbl[,id_num] <- map_df(tbl[,id_num], as.numeric)
+  }
+
+  return(tbl)
+}
+
+#' Read a SWAT+ connecitivity (*.con) input file.
+#'
+#' @param file_path Path of the SWAT+ input file.
+#'
+#' @returns The connecitivity input file as a tibble.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr across mutate
+#' @importFrom purrr set_names
+#' @importFrom stringr str_trim str_split
+#' @importFrom tibble as_tibble
+#' @importFrom tidyselect matches starts_with
+#'
+#' @keywords internal
+#'
+read_con_file <- function(file_path) {
+  obj_names <- c("id", "name", "gis_id", "area", "lat", "lon", "elev",
+                 "obj_id", "wst", "cst", "ovfl", "rule", "out_tot")
+  con_names <- c("obj_typ", "obj_id", "hyd_typ", "frac")
+
+  if(file.exists(file_path)) {
+    con_mtx <- fread(file_path, skip = 2, sep = NULL, sep2 = NULL, header = F) %>%
+      unlist(.) %>%
+      unname(.) %>%
+      str_trim(.) %>%
+      str_split(., '[:space:]+', simplify = T)
+
+    n_con <- (dim(con_mtx)[2]-length(obj_names)) / length(con_names)
+    if(n_con > 0) {
+      rep_ids <- 1:n_con
+    } else {
+      rep_ids <- NULL
+    }
+
+    con_names <- paste(rep(con_names, n_con),
+                       rep(rep_ids, each = length(con_names)),
+                       sep = '_')
+
+    col_types <- unlist(strsplit(c('iciddddiciiii', rep('cicd', n_con)), '')) %>%
+      recode(., c = 'character', d = 'numeric', i = 'integer')
+
+    con_tbl <- as_tibble(con_mtx, validate = NULL,
+                         .name_repair = ~ c(obj_names, con_names)) %>%
+      map2_df(., col_types, ~ as(.x, .y))
+
+    # id_int <- c(1,3,8,13, 15 + (rep_ids - 1)*4)
+    # con_tbl[ , id_int] <- map_df(con_tbl[ , id_int], as.integer)
+    #
+    # id_dbl <- c(4:7, 17 + (rep_ids - 1)*4)
+    # con_tbl[ , id_dbl] <- map_df(con_tbl[ , id_dbl], as.numeric)
+  } else {
+    con_tbl <- tibble(!!!rep(NA, length(obj_names)),
+                      .rows = 0, .name_repair = ~ obj_names)
+
+    col_types <- unlist(strsplit('iciddddiciiii', '')) %>%
+      recode(., c = 'character', d = 'numeric', i = 'integer')
+
+    con_tbl <- map2_df(con_tbl, col_types, ~ as(.x, .y))
+  }
+
+  return(con_tbl)
+}
+
+#' Add a running ID to duplicated names
+#'
+#' @param col_name Character vector of column names
+#'
+#' @returns the `col_name` character vector with IDs for duplicated names
+#'
+#' @keywords internal
+#'
+add_suffix_to_duplicate <- function(col_name){
+  dupl <- table(col_name) %>%
+    .[. > 1]
+
+  if(length(dupl > 0)) {
+    for(i in 1:length(dupl)) {
+      col_name[col_name == names(dupl[i])] <-
+        paste0(names(dupl[i]), c('', 1:(dupl[i]-1)))
+    }
+  }
+
+  return(col_name)
+}
+
+#' Transform x to a matrix with n columns and fill up with NA values
+#'
+#' @param x character vector or NULL
+#' @param n Number of elements
+#'
+#' @keywords internal
+#'
+as_mtx_null <- function(x, n) {
+  if(is.null(x)) {
+    matrix(rep(NA_character_, 7), ncol = n)
+  } else {
+    matrix(x, nrow = n) %>%
+      t(.)
+  }
+}
---
+++ R/read_swat.R
@@ -0,0 +1,171 @@
+
+#' Read SWAT+ input and output files.
+#'
+#' @param file_path Path to the SWAT+ file.
+#'
+#' @returns The SWAT+ input or output file in a tibble.
+#'
+#' @export
+#'
+read_swat <- function(file_path) {
+  file_name <- basename(file_path)
+  dir_path  <- dirname(file_path)
+
+  read_type <- lookup_read_type(file_name)
+
+  if (!file_name %in% list.files(path = dir_path)) {
+    stop("File with the name '", file_name, "' was not found in\n",
+         dir_path)
+  }
+
+  if(is.null(read_type[[1]])) {
+    stop("File with the name '", file_name, "' is not supported!")
+  } else if (read_type$type == 'tbl') {
+    tbl <- read_tbl(file_path, n_skip = read_type$n_skip)
+  } else if (read_type$type == 'tbl2') {
+    tbl <- read_tbl2(file_path = file_path,
+                     def_names = read_type$def_names,
+                     par_names = read_type$par_names,
+                     id_num = read_type$id_num)
+  } else if (read_type$type == 'con') {
+    tbl <- read_con_file(file_path)
+  } else if (read_type$type == 'not') {
+    stop("'", file_name, "' is not implemented yet!")
+  }
+
+  return(tbl)
+
+}
+
+#' Look up the function type and additional input arguments to read a SWAT+ file.
+#'
+#' @param file_name Path to the SWAT+ file.
+#'
+#' @returns A list of arguments:
+#'  - `type` is used to select the read function for the file.
+#'  - `n_skip` and `has_unit` as arguments for the function `read_tbl`
+#'  - or `def_names`, `par_names` and `id_num` as arguments for the
+#'    function `read_tbl2`
+#'  - `n_skip` as arguments for the function `read_tbl_n`
+#'
+#' @keywords internal
+#'
+lookup_read_type <- function(file_name) {
+  file_sfx  <- substr(file_name, nchar(file_name) - 2, nchar(file_name))
+
+  if (file_name %in% c('crop_yld_yr.txt', 'crop_yld_yr.txt',
+                       'hydcon.out', 'mgt.out')) {
+    stop("'", file_name, "' is not implemented yet!")
+  } else if (file_name %in% c('files_out.out', 'flow_duration_curve.out')) {
+    file_name <- 'out_without_units'
+  } else if (file_sfx %in% c('txt', 'out')) {
+    file_name <- 'out_with_units'
+  }
+
+  read_lookup <- list(
+    # Input files
+    'aqu_catunit.ele'   = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'aqu_cha.lin'       = list(type = 'not'),
+    'aquifer.aqu'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'aquifer.con'       = list(type = 'con'),
+    'atmodep.cli'       = list(type = 'not'),
+    'bmpuser.str'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'cal_parms.cal'     = list(type = 'tbl', n_skip = 2), #*** add value when write
+    'calibration.cal'   = list(type = 'not'),
+    'chandeg.con'       = list(type = 'con', n_skip = 1, has_unit = FALSE),
+    'channel-lte.cha'   = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'chem_app.ops'      = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'cntable.lum'       = list(type = 'tbln', n_skip = 1, has_unit = FALSE),
+    'codes.bsn'         = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'cons_practice.lum' = list(type = 'tbln', n_skip = 1),
+    'exco.con'          = list(type = 'con', n_skip = 1, has_unit = FALSE),
+    'fertilizer.frt'    = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'field.fld'         = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'file.cio'          = list(type = 'not'),
+    'filterstrip.str'   = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'fire.ops'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'grassedww.str'     = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'graze.ops'         = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'harv.ops'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'hmd.cli'           = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'hru.con'           = list(type = 'con', n_skip = 1, has_unit = FALSE),
+    'hru-data.hru'      = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'hydrology.hyd'     = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'hydrology.res'     = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'hydrology.wet'     = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'hyd-sed-lte.cha'   = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'initial.aqu'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'initial.cha'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'initial.res'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'irr.ops'           = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'landuse.lum'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'ls_unit.def'       = list(type = 'not'), #*** add value when write
+    'ls_unit.ele'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'lum.dtl'           = list(type = 'not'),
+    'management.sch'    = list(type = 'tbl2',
+                               def_names = c('name', 'numb_ops', 'numb_auto'),
+                               par_names = c('op_typ', 'mon', 'day', 'hu_sch',
+                                             paste0('op_data', 1:3)),
+                               id_num    =  c(2:3, 5:7, 10)),
+    'nutrients.cha'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'nutrients.res'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'nutrients.sol'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'object.cnt'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'object.prt'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'om_water.ini'        = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'ovn_table.lum'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'parameters.bsn'      = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'path_hru.ini'        = list(type = 'not'),
+    'pcp.cli'             = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'pest_hru.ini'        = list(type = 'not'),
+    'pesticide.pes'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'plant.ini'           = list(type = 'tbl2',
+                                 def_names = c('pcom_name', 'plt_cnt', 'rot_yr_ini'),
+                                 par_names = c('plt_name', 'lc_status', 'lai_init',
+                                               'bm_init', 'phu_init', 'plnt_pop',
+                                               'yrs_init', 'rsd_init'),
+                                 id_num    =  c(2:3, 6:11)),
+    'plants.plt'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'print.prt'           = list(type = 'not'),
+    'res_rel.dtl'         = list(type = 'not'),
+    'reservoir.con'       = list(type = 'con'),
+    'reservoir.res'       = list(type = 'tbl'),
+    'rout_unit.con'       = list(type = 'con'),
+    "rout_unit.def"       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'rout_unit.ele'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'rout_unit.rtu'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'salt_aqu.ini'        = list(type = 'not'),
+    'salt_channel.ini'    = list(type = 'not'),
+    'salt_hru.ini'        = list(type = 'not'),
+    'sediment.res'        = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'septic.sep'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'septic.str'          = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'slr.cli'             = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'snow.sno'            = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'soil_plant.ini'      = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'soils.sol'           = list(type = 'tbl2',
+                                 def_names = c('name', 'nly', 'hyd_grp', 'dp_tot',
+                                               'anion_excl', 'perc_crk', 'texture'),
+                                 par_names = c('dp', 'bd', 'awc', 'soil_k',
+                                               'carbon', 'clay', 'silt', 'sand',
+                                               'rock', 'alb', 'usle_k', 'ec',
+                                               'caco3', 'ph'),
+                                 id_num    =  c(2, 4:6, 8:21)),              # !!! Is tbl2 but have to adapt for soil
+    'sweep.ops'           = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'tiledrain.str'       = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'tillage.til'         = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'time.sim'            = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'tmp.cli'             = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'topography.hyd'      = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'treatment.trt'       = list(type = 'not'),
+    'urban.urb'           = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'weather-sta.cli'     = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'weather-wgn.cli'     = list(type = 'not'),
+    'wetland.wet'         = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    'wnd.cli'             = list(type = 'tbl', n_skip = 1, has_unit = FALSE),
+    # Output files
+    'out_with_units'      = list(type = 'tbl', n_skip = 1, has_unit = TRUE),
+    'out_without_units'      = list(type = 'tbl', n_skip = 1, has_unit = TRUE)
+    )
+  return(read_lookup[[file_name]])
+}
